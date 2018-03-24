ab02163ef9615e090f4d41d0df21b16f39402da6
lebeerman <omnidbeerman@gmail.com>
2018-03-24 15:10:38 -0600

    :zap: connecting a mongoDB to EC2 to catch weather data - test

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+var MongoClient = require('mongodb').MongoClient;
+var uri = "mongodb+srv://lebeerman:" + process.env.MONGO_PWD + "@cluster0.mongodb.net/test";
 
 const port = process.env.PORT || 3000;
 
@@ -14,12 +17,27 @@ app.use(cors());
 let data = {};
 
 app.get("/", (req, res) => {
-  res.send(data);
+  MongoClient.connect(uri, function(err, client) {
+    const collection = client.db('test').collection('test');
+    let example = collection.find({});
+    console.log(example);
+    res.send(example);
+    // perform actions on the collection object
+    client.close();
+  });
 });
 
 app.post("/", (req, res) => {
   data = req.body;
   res.sendStatus(200);
+
+  MongoClient.connect(uri, function(err, client) {
+      const collection = client.db("test").collection("devices");
+      // perform actions on the collection object
+      client.close();
+  });
 });
+
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
