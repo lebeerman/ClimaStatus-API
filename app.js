85bb5d8370831015897a9f658e834dccab6fff3b
lebeerman <omnidbeerman@gmail.com>
2018-03-25 14:29:33 -0600

    Local dev branch

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const cors = require('cors');
+const morgan = require('morgan');
 const app = express();
 const server = http.createServer(app);
 const port = parseInt(process.env.PORT || 3000);
+const devMode = process.env.NODE_ENV !== 'production';
 
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://lebeerman:${process.env.MONGO_PWD}@cluster0-ojwct.mongodb.net/test`;
+const assert = require('assert');
+const uri = (port === 3000) ? `mongodb://localhost:27017/localTest` : `mongodb+srv://lebeerman:${process.env.MONGO_PWD}@cluster0-ojwct.mongodb.net/test`;
 const path = require('path');
 
 app.use(helmet());
@@ -18,36 +21,89 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan(devMode ? 'dev' : 'combined'));
 app.use(cors({ origin: true }));
+// app.use(notFound);
+// app.use(errorHandler);
 
 let data = {};
 
-app.get('/', (req, res) => {
-  MongoClient.connect(uri, (err, client) => {
-    // Doing stuff
-    if (err) console.error(err);
-    const collection = client.db('test').collection('test');
-    // pull the most recent item hopefully
-    const mostRecent = collection.find().sort({"dateutc": -1}).limit(1);
-    res.send(mostRecent);
-    console.log('SENTIT', items);
-    res.sendStatus(200);
-    client.close();
-  })
-  .catch(err => console.error(err));
-  res.status(303).send({message: 'something went wrong'});
+// ROUTES
+app.get('/', (req, res, next) => {
+  MongoClient
+    .connect(uri, (err, client) => {
+      if (err) return console.dir(err);
+      else console.log('Connected');
+      const collection = client.db('localTest').collection('collection');
+      collection.find().sort({dateutc: -1}).toArray((err, payloads) => {
+        if (err) console.log('FIND', err);
+        const mostRecentDate = orderByDate(payloads, 'dateutc');
+        res.status(200).send(mostRecentDate);
+      });
+      client.close();
+    })
 });
 
-app.post('/', (req, res) => {
+app.post('/', (req, res, next) => {
   data = req.body;
-  MongoClient.connect(uri, (err, client) => {
-    if (err) console.log('POST Error: ', err);
-    const collection = client.db('test').collection('test');
-    collection.insertOne(data);
-    console.log('ADDING', data);
-  })
-  .catch(err => console.error(err));
-  res.sendStatus(200);
+  MongoClient
+    .connect(uri, (err, client) => {
+      if (err) console.log('POST Error: ', err);
+      const collection = client.db('localTest').collection('collection');
+      try {
+        collection.insertOne(data);
+        console.log('ADDING', data);
+        client.close();
+      } catch (err) {
+        console.log(err);
+      }
+    })
+  res.send('Sensor Data Saved');
 });
+
+app.get('/payload', (req, res, next) => {
+  MongoClient.connect(uri, (err, client) => {
+    if (err) return console.dir(err);
+    else console.log('Connected');
+    const totalPayloads = client.db('localTest').collection('collection').count((err, count) => {
+      if (err) throw err;
+      console.log('Total Rows: ' + count);
+      return count;
+    });
+    res.send(totalPayloads);
+    client.close();
+  });
+});
+
+
+//HELPER FUNCTIONS
+
+const orderByDate = (arr, dateProp) => {
+  return arr.slice().sort(function(a, b) {
+    return a[dateProp] < b[dateProp] ? -1 : 1;
+  })[0];
+}
+
+// const insertPayloads = (db, callback) => {
+//   // Get the documents collection
+//   const collection = db.db('localTest').collection('collection');
+//   // Insert some documents
+//   collection.insertMany([PAYLOAD AS ARRAY], (err, result) => {
+//     assert.equal(err, null); // dev testing
+//     console.log('Inserted 3 documents into the collection');
+//     callback(result);
+//   });
+// };
+
+// const findPayloads = (db, callback) => {
+//   // Get the documents collection
+//   const collection = db.db('localTest').collection('collection');
+//   // Find some documents
+//   collection.find({}).toArray((err, docs) => {
+//     assert.equal(err, null);
+//     console.log('Found the following records');
+//     console.log(docs);
+//     callback(docs);
+//   });
+// };
 
 server
   .listen(port)
